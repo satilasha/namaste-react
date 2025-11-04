48b692cd86e77200d1ad06e993c8378415da4e73
satilasha <swatighosh2111@gmail.com>
2025-11-04 23:33:20 +0530

    episode-1

---
+++ app.js
@@ -0,0 +1,16 @@
+let parent = React.createElement("div", {"id" : "parent"}, 
+    React.createElement("div", {"id" : "child"},
+        [React.createElement("h1", {}, "I am h1 tag"),
+         React.createElement("h2", {}, "I am h2 tag")]
+    )
+)
+
+
+
+
+
+
+let heading = React.createElement("h1", {"id" : "heading"}, "Hello World from React");
+
+let root = ReactDOM.createRoot(document.getElementById("root"));
+root.render(parent);
