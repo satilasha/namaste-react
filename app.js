f2b7bc9c13f726593a1523d01525044ed121dd18
satilasha <swatighosh2111@gmail.com>
2025-11-12 00:15:48 +0530

    episode-3

--- app.js
+++ app.js
@@ -2,15 +2,29 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 
 
-let parent = React.createElement("div", { "id": "parent" },
-    React.createElement("div", { "id": "child" },
-        [React.createElement("h1", {}, "I am h1 hiiiiii tag"),
-        React.createElement("h2", {}, "I am h2 tag")]
-    )
-)
-
 
 let heading = React.createElement("h1", { "id": "heading" }, "Hello World from React");
 
+let Jsxheading = () => (
+    <h1 className="heading">
+        Nameaste React using JSX
+    </h1 >
+)
+
+let title = (
+    <h1 className="heading">
+        Nameaste React using react element
+    </h1 >
+)
+
+const ReactComponent = () => (
+    <div>
+        {title}
+        < Jsxheading />
+        <h1>React Component</h1>
+    </div>
+
+)
+
 let root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(parent);
+root.render(<ReactComponent />);
